2e449dcf8029bd491d71050973dbff3f3a5a81a8
Morgan Creekmore <morgan@creekmore.email>
2021-08-19 20:43:28 -0500

    Change emoji

--- src/CoachFrank.Commands/UtilCommands.cs
+++ src/CoachFrank.Commands/UtilCommands.cs
@@ -16,7 +16,7 @@ namespace CoachFrank.Commands
         [Command("ping")]
         public Task Ping(CommandContext ctx)
         {
-            return ctx.RespondAsync(":ping_pong:");
+            return ctx.RespondAsync(EmojiConstants.PingPong);
         }
 
         [Command("uptime")]
@@ -36,31 +36,31 @@ namespace CoachFrank.Commands
             try
             {
                 var statusUpdate = await skateboard3Client.GetStatus();
-                statusUpdateResult.AppendLine($"{EmojiConstants.CheckMark} Skateboard3Server is Up for {(DateTime.Now - statusUpdate.StartTime).Humanize(2)}");
+                statusUpdateResult.AppendLine($"{EmojiConstants.GreenCircle} Skateboard3Server is Up for {(DateTime.Now - statusUpdate.StartTime).Humanize(2)}");
             }
             catch (Exception)
             {
-                statusUpdateResult.AppendLine($"{EmojiConstants.X} Skateboard3Server is Down");
+                statusUpdateResult.AppendLine($"{EmojiConstants.RedCircle} Skateboard3Server is Down");
             }
             var qos1Client = RestClient.For<IServerStatus>("http://sb3-qs1.hallofmeat.net:17502");
             try
             {
                 var statusUpdate = await qos1Client.GetStatus();
-                statusUpdateResult.AppendLine($"{EmojiConstants.CheckMark} Skateboard3Server QS1 is Up for {(DateTime.Now - statusUpdate.StartTime).Humanize(2)}");
+                statusUpdateResult.AppendLine($"{EmojiConstants.GreenCircle} Skateboard3Server QS1 is Up for {(DateTime.Now - statusUpdate.StartTime).Humanize(2)}");
             }
             catch (Exception)
             {
-                statusUpdateResult.AppendLine($"{EmojiConstants.X} Skateboard3Server QS1 is Down");
+                statusUpdateResult.AppendLine($"{EmojiConstants.RedCircle} Skateboard3Server QS1 is Down");
             }
             var qos2Client = RestClient.For<IServerStatus>("http://sb3-qs2.hallofmeat.net:17502");
             try
             {
                 var statusUpdate = await qos2Client.GetStatus();
-                statusUpdateResult.AppendLine($"{EmojiConstants.CheckMark} Skateboard3Server QS2 is Up for {(DateTime.Now - statusUpdate.StartTime).Humanize(2)}");
+                statusUpdateResult.AppendLine($"{EmojiConstants.GreenCircle} Skateboard3Server QS2 is Up for {(DateTime.Now - statusUpdate.StartTime).Humanize(2)}");
             }
             catch (Exception)
             {
-                statusUpdateResult.AppendLine($"{EmojiConstants.X} Skateboard3Server QS2 is Down");
+                statusUpdateResult.AppendLine($"{EmojiConstants.RedCircle} Skateboard3Server QS2 is Down");
             }
 
             await ctx.RespondAsync(statusUpdateResult.ToString());
--- src/CoachFrank.Commands/Utils/EmojiConstants.cs
+++ src/CoachFrank.Commands/Utils/EmojiConstants.cs
@@ -2,7 +2,10 @@
 {
     public static class EmojiConstants
     {
-        public static string CheckMark = ":white_check_mark:";
-        public static string X = ":x:";
+        public static string Check = ":white_check_mark:";
+        public static string Cross = ":x:";
+        public static string PingPong = ":ping_pong:";
+        public static string RedCircle = ":red_circle:";
+        public static string GreenCircle = ":green_circle:";
     }
 }
